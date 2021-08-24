505166a0372158e42e34c4dc98e16d1a4486b3be
gou manabe <86933401+goumanabe@users.noreply.github.com>
2021-08-24 21:50:43 +0900

    quize upload
    クイズをこっちに移動させてみた。起動できればありがたい！

---
+++ quiz.huuhin/app copy完成形.js
@@ -0,0 +1,75 @@
+const quiz = [
+  {
+    question: '楓浜が生まれた日はいつ？',
+    answers: [ '11月22日', '1月11日', '10月22日', '12月11日'],
+    correct: '11月22日'
+  }, {
+    question: '楓浜の生まれた時の体重は何g？',
+    answers: [ '１２３g', '１５７g', '１９９g', '３５４g'],
+    correct: '１５７g'
+  }, {
+    question: '楓浜のお母さんの名前は？',
+    answers: [ '永明', '桜浜', '桃浜', '良浜'],
+    correct: '良浜'
+  }
+];
+
+const $window = window;
+const $doc = document;
+const $question = $doc.getElementById('js-question');
+const $buttons = $doc.querySelectorAll('.btn');
+
+const quizLen = quiz.length;
+let quizCount = 0;
+let score = 0;
+
+const init = () => {
+  $question.textContent = quiz[quizCount].question;
+  
+  const buttonLen = $buttons.length;
+  let btnIndex = 0;
+  
+  while(btnIndex < buttonLen){
+    $buttons[btnIndex].textContent = quiz[quizCount].answers[btnIndex];
+    btnIndex++;
+  }
+};
+
+const goToNext = () => {
+  quizCount++;
+  if(quizCount < quizLen){
+    init(quizCount);
+  } else {
+    // $window.alert('クイズ終了！');
+    showEnd();
+  }
+};
+
+const judge = (elm) => {
+  if(elm.textContent === quiz[quizCount].correct){
+    $window.alert('正解!');
+    score++;
+  } else {
+    $window.alert('不正解!');
+  }
+  goToNext();
+};
+
+const showEnd = () => {
+  $question.textContent = '終了！あなたのスコアは' + score + '/' + quizLen + 'です';
+  
+  const $items = $doc.getElementById('js-items');
+  $items.style.visibility = 'hidden';
+};
+
+init();
+
+let answersIndex = 0;
+let answersLen = quiz[quizCount].answers.length;
+
+while(answersIndex < answersLen){
+  $buttons[answersIndex].addEventListener('click', (e) => {
+    judge(e.target);
+  });
+  answersIndex++;
+}

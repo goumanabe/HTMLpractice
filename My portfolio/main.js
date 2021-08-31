0073ca68e2ac20bde5bd8349a7df8cd9ebdb50c4
gou manabe <86933401+goumanabe@users.noreply.github.com>
2021-08-31 23:19:06 +0900

    javascript file
    ドットインストールのjavascriptのデータをいれます

---
+++ My portfolio/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function process() {
+  document.getElementById('target').textContent = 'めっちゃ真面目です。';
+  document.getElementById('torigger').classList.add('changed');
+}
+
+  document.getElementById('torigger').addEventListener('click',process);
+  // クリックされたら
+
+// console.log('hello');
+// console.log("hello");
+// console.log('it's me!');
+// console.log("it's me!");
+// console.log('it's me!');

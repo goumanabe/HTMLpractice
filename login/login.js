431d945c9f1f2bf097e82cc2e8e200be33686d31
gou manabe <86933401+goumanabe@users.noreply.github.com>
2021-09-06 07:35:48 +0900

    login folder upp
    ログインフォルダ、Javascript作成中
    ログイン承認のやつ作りたい

---
+++ login/login.js
@@ -0,0 +1,32 @@
+let ck = document.getElementById('check');
+
+let img =document.getElementById('quiz');
+let attrs = quiz.attributes;
+
+let gazo = document.createElement('img');
+ck.addEventListener('click',function(){
+  // window.alert('本当に人間ですか？');
+  attrs[0].value='tako.png';
+  // console.log(attrs[0].value);
+  let a = attrs[0].value;
+
+
+  gazo.src =  a;
+  let list =document.getElementById('list');
+  list.appendChild(gazo);
+
+});
+// width="200px" height="200px"
+//画像をクリックすると回数を数えてくれる
+// ログインの画像商人みたいなやつで、画像を表示して、その画像に点数がついてて、それ以上の点数になったら、ログインできるようにするのを作りたい
+let sum = 0;
+quiz.addEventListener('click',function(){
+  sum++;
+  // console.log(sum);
+  if(sum>0){
+
+  }else{
+    
+  }
+});
+
